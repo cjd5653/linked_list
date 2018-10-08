18c573440b2ec5f1eed94ddfbf0924049c0945cf
Connor <43938155+cjd5653@users.noreply.github.com>
2018-10-08 14:34:08 -0400

    Created application.cxx
    Contains main function for testing purposes.

---
+++ application.cxx
@@ -0,0 +1,14 @@
+// Connor Deardorff
+// 10/7/18
+// Description: Contains a main function used to test my implementation of
+// 				a doubly-linked list.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Main function currently empty." << endl;
+	
+	return 0;
+}
